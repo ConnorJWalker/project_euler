efde30646c584e2cbbe3177b0927fe0189297444
ConnorJWalker <connorjwalker32@hotmail.co.uk>
2018-10-02 12:40:24 +0100

    added file reader class

---
+++ ProjectEuler/FileReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace ProjectEuler
+{
+    public class FileReader
+    {
+        public string ReadFile(string fileName) => 
+            File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}../../TextFiles/{fileName}.txt");
+
+        public string[] ReadFileArray(string fileName) => 
+            File.ReadAllLines($"{AppDomain.CurrentDomain.BaseDirectory}../../TextFiles/{fileName}.txt");
+    }
+}
